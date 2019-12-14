a25a08d2711d74084735f65b2b99349b0f10cfaf
KrimsN <deni.tim99@yandex.ru>
2019-12-14 13:17:36 +0700

    Updates

---
+++ precache-manifest.1994b4e8f54779d83f67ecce8f7c319b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bf6ba2e8cf575210483e2991787304b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7850a78fd8c15041d646",
+    "url": "./static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "18304883f57912afa9e3",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "7850a78fd8c15041d646",
+    "url": "./static/js/2.1a59c3c0.chunk.js"
+  },
+  {
+    "revision": "ce028d5e2b0507b0fb507336501c1edf",
+    "url": "./static/js/2.1a59c3c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "18304883f57912afa9e3",
+    "url": "./static/js/main.6048c5a6.chunk.js"
+  },
+  {
+    "revision": "40be310bb6028c08825c",
+    "url": "./static/js/runtime-main.2edb1291.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
